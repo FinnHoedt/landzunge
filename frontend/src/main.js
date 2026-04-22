47be77434780092d9e0fcadd20e696848ca313fe
Finn Hoedt <finnhoedt@gmx.de>
2026-04-22 08:26:18 +0200

    feat: add dispatches module and multi-page Vite build config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,11 @@ import { initSound } from './sound.js'
 import { initGuestbook } from './guestbook.js'
 import { initTracker } from './tracker.js'
 import { initWeather } from './weather.js'
+import { initDispatchesTeaser } from './dispatches.js'
 
 initI18n()
 initSound()
 initGuestbook()
 initTracker()
 initWeather()
+initDispatchesTeaser()
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,12 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({})
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        dispatches: 'dispatches.html',
+      },
+    },
+  },
+})
